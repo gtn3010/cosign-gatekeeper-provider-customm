bc5c4659442d878145581bba6e859959e2b28e7a
Nguyen GT <nguyengt@vpbank.com.vn>
2023-03-09 18:36:17 +0700

    Add more checking options (identity issuer, tlog verify, SCT verify...)

--- provider.go
+++ provider.go
@@ -15,21 +15,26 @@
 package main
 
 import (
+	"context"
 	"crypto/x509"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"os"
+	"strconv"
 	"time"
 
 	"github.com/google/go-containerregistry/pkg/name"
 	"github.com/open-policy-agent/frameworks/constraint/pkg/externaldata"
 
-	"github.com/sigstore/cosign/cmd/cosign/cli/fulcio"
-	"github.com/sigstore/cosign/cmd/cosign/cli/options"
-	"github.com/sigstore/cosign/cmd/cosign/cli/rekor"
-	"github.com/sigstore/cosign/pkg/cosign"
+	"github.com/sigstore/cosign/v2/cmd/cosign/cli/fulcio"
+	"github.com/sigstore/cosign/v2/cmd/cosign/cli/initialize"
+	"github.com/sigstore/cosign/v2/cmd/cosign/cli/options"
+	"github.com/sigstore/cosign/v2/cmd/cosign/cli/rekor"
+	"github.com/sigstore/cosign/v2/pkg/cosign"
 	"github.com/sigstore/rekor/pkg/generated/client"
+	"github.com/sigstore/sigstore/pkg/cryptoutils"
 )
 
 const (
@@ -41,13 +46,31 @@ var (
 	rekorClient         *client.Rekor
 	fulcioRoots         *x509.CertPool
 	fulcioIntermediates *x509.CertPool
+	identities          []cosign.Identity
+	skipTlogVerify      bool
+	ignoreSCT           bool
 )
 
 func main() {
 	fmt.Println("starting server...")
 	http.HandleFunc("/validate", validate)
 
-	rc, err := rekor.NewClient(defaultRekorURL)
+	// Init self-service TrustRoot if needed
+	if os.Getenv("TUF_URL") != "" {
+		mirror := os.Getenv("TUF_URL")
+		fmt.Println("Starting to initialize the Trust root from", mirror)
+		root := mirror + "/root.json"
+		err := initialize.DoInitialize(context.Background(), root, mirror)
+		if err != nil {
+			panic(fmt.Sprintf("initializing selfhosted TrustRoot: %v", err))
+		}
+	}
+
+	rekorURL := os.Getenv("REKOR_URL")
+	if rekorURL == "" {
+		rekorURL = defaultRekorURL
+	}
+	rc, err := rekor.NewClient(rekorURL)
 	if err != nil {
 		panic(fmt.Sprintf("creating Rekor client: %v", err))
 	}
@@ -65,6 +88,27 @@ func main() {
 	}
 	fulcioIntermediates = intermediates
 
+	// Claims from Identity token to verify (compulsory when using keyless verifying)
+	identities = []cosign.Identity{{
+		Issuer:        os.Getenv("CERT_OIDC_ISSUER"),
+		Subject:       os.Getenv("CERT_OIDC_SUBJECT"),
+		IssuerRegExp:  os.Getenv("CERT_OIDC_ISSUER_REGEX"),
+		SubjectRegExp: os.Getenv("CERT_OIDC_SUBJECT_REGEX"),
+	}}
+
+	skipTlogVerify, err = strconv.ParseBool(os.Getenv("SKIP_TLOG_VERIFY"))
+	if err != nil {
+		panic(fmt.Sprintf("wrong value type Bool for env SKIP_TLOG_VERIFY: %v", err))
+	}
+
+	ignoreSCT, err = strconv.ParseBool(os.Getenv("IGNORE_SCT"))
+	if err != nil {
+		panic(fmt.Sprintf("wrong value type Bool for env IGNORE_SCT: %v", err))
+	}
+
+	tlsKeyPath := os.Getenv("TLS_KEY_PATH")
+	tlsCertPath := os.Getenv("TLS_CERT_PATH")
+
 	srv := &http.Server{
 		Addr:              ":8090",
 		ReadTimeout:       10 * time.Second,
@@ -72,7 +116,7 @@ func main() {
 		ReadHeaderTimeout: 10 * time.Second,
 	}
 
-	if err := srv.ListenAndServe(); err != nil {
+	if err := srv.ListenAndServeTLS(tlsCertPath, tlsKeyPath); err != nil {
 		panic(err)
 	}
 }
@@ -102,11 +146,44 @@ func validate(w http.ResponseWriter, req *http.Request) {
 	results := make([]externaldata.Item, 0)
 
 	ctx := req.Context()
-	ro := options.RegistryOptions{}
+	// Options connecting to registry
+	ro := options.RegistryOptions{
+		KubernetesKeychain: true, // Allow to support getting credential with some other registry providers
+		AllowInsecure:      true,
+	}
 	co, err := ro.ClientOpts(ctx)
 	if err != nil {
 		sendResponse(nil, fmt.Sprintf("ERROR: %v", err), w)
 		return
+	}
+
+	checkOpt := &cosign.CheckOpts{
+		Identities:         identities,
+		RekorClient:        rekorClient,
+		RegistryClientOpts: co,
+		RootCerts:          fulcioRoots,
+		IntermediateCerts:  fulcioIntermediates,
+		ClaimVerifier:      cosign.SimpleClaimVerifier,
+	}
+
+	// Tlog verifying if enabled
+	if skipTlogVerify {
+		checkOpt.IgnoreSCT = true
+	} else {
+		checkOpt.RekorPubKeys, err = cosign.GetRekorPubs(ctx)
+		if err != nil {
+			sendResponse(nil, fmt.Sprintf("getting Rekor public keys: %v", err), w)
+			return
+		}
+	}
+
+	// CTlog verifying if enabled
+	if !ignoreSCT {
+		checkOpt.CTLogPubKeys, err = cosign.GetCTLogPubs(ctx)
+		if err != nil {
+			sendResponse(nil, fmt.Sprintf("getting ctlog public keys: %v", err), w)
+			return
+		}
 	}
 
 	// iterate over all keys
@@ -118,13 +195,7 @@ func validate(w http.ResponseWriter, req *http.Request) {
 			return
 		}
 
-		checkedSignatures, bundleVerified, err := cosign.VerifyImageSignatures(ctx, ref, &cosign.CheckOpts{
-			RekorClient:        rekorClient,
-			RegistryClientOpts: co,
-			RootCerts:          fulcioRoots,
-			IntermediateCerts:  fulcioIntermediates,
-			ClaimVerifier:      cosign.SimpleClaimVerifier,
-		})
+		checkedSignatures, bundleVerified, err := cosign.VerifyImageSignatures(ctx, ref, checkOpt)
 
 		if err != nil {
 			fmt.Println(err)
@@ -132,12 +203,25 @@ func validate(w http.ResponseWriter, req *http.Request) {
 			return
 		}
 
+		listSANS := make([]string, 0)
 		if bundleVerified {
 			fmt.Println("signature verified for:", key)
 			fmt.Printf("%d number of valid signatures found for %s, found signatures: %v\n", len(checkedSignatures), key, checkedSignatures)
+
+			for _, sig := range checkedSignatures {
+				cert, err := sig.Cert()
+				if err != nil {
+					fmt.Println(err)
+				}
+				sans := getSubjectAlternateNames(cert)
+				fmt.Printf("SANs from %s: %v", key, sans)
+				listSANS = append(listSANS, sans...)
+			}
+
 			results = append(results, externaldata.Item{
 				Key:   key,
-				Value: key + "_valid",
+				Value: listSANS,
+				// Value: key + "_valid",
 			})
 		} else {
 			fmt.Printf("no valid signatures found for: %s\n", key)
@@ -168,4 +252,22 @@ func sendResponse(results *[]externaldata.Item, systemErr string, w http.Respons
 	if err := json.NewEncoder(w).Encode(response); err != nil {
 		panic(err)
 	}
+}
+
+func getSubjectAlternateNames(cert *x509.Certificate) []string {
+	sans := []string{}
+	sans = append(sans, cert.DNSNames...)
+	sans = append(sans, cert.EmailAddresses...)
+	for _, ip := range cert.IPAddresses {
+		sans = append(sans, ip.String())
+	}
+	for _, uri := range cert.URIs {
+		sans = append(sans, uri.String())
+	}
+	// ignore error if there's no OtherName SAN
+	otherName, _ := cryptoutils.UnmarshalOtherNameSAN(cert.Extensions)
+	if len(otherName) > 0 {
+		sans = append(sans, otherName)
+	}
+	return sans
 }
